1991c03fef01f73728938a9ad59695d25be0e724
Zzifi <yakovmuxin@mail.ru>
2023-01-22 16:41:37 +0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/gitTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gitTest.dir/main.cpp.obj"
+  "gitTest.exe"
+  "gitTest.exe.manifest"
+  "gitTest.pdb"
+  "libgitTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gitTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
